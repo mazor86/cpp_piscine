aff62b6be2925407fb6a804033d14ed04ab9ef8c
mazor <mazor@student.21-school.ru>
2021-04-19 16:50:09 +0300

    rm unnecessary header in day00ex00

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -6,15 +6,14 @@
 /*   By: mazor <mazor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 09:30:50 by mazor             #+#    #+#             */
-/*   Updated: 2021/04/19 10:13:34 by mazor            ###   ########.fr       */
+/*   Updated: 2021/04/19 16:49:17 by mazor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <ctype.h>
 
-void	get_loud(std::string str)
+void	get_louder(std::string str)
 {
 	for (size_t i = 0; i < str.length(); i++)
 		std::cout << (char)toupper(str[i]);
@@ -29,7 +28,7 @@ int		main(int ar, char **av)
 	{
 		while (i < ar)
 		{
-			get_loud(av[i]);
+			get_louder(av[i]);
 			i++;
 		}
 	}
